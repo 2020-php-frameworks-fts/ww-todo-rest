45a4fdf9d4858b2d9a30868f33e7eee29e0f5ba7
w.brzuszkiewicz <wojtas1146@gmail.com>
2021-05-11 17:10:16 +0200

    Working app

---
+++ login.php
@@ -0,0 +1,62 @@
+<?php
+// 201 - empty data
+// 202 - mysql connection err
+// 203 - wrong password
+// 200 successfull
+
+if(isset($_SERVER["HTTP_ORIGIN"]))
+{
+    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+else
+{
+    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+    header("Access-Control-Allow-Origin: *");
+}
+
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 600");    // cache for 10 minutes
+
+if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
+{
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    //Just exit with 200 OK with the above headers for OPTIONS method
+    exit(0);
+}
+
+$login = $_POST["login"];
+$password = $_POST["password"];
+
+if($login == null && $password == null) {
+    http_response_code(201);
+    die();
+}
+
+// select query
+$query = "SELECT login, password FROM users WHERE login = \"${login}\" AND password = \"{$password}\"";
+
+$mysqlConnection = new mysqli("host","login","password","database");
+if($mysqlConnection -> connect_errno) {
+    http_response_code(202);
+    die();
+}
+
+$result = $mysqlConnection->query($query);
+$data = $result->fetch_array(MYSQLI_NUM);
+
+if($data == null) {
+    http_response_code(203);
+    $mysqlConnection->close();
+    die();
+}
+
+$mysqlConnection->close();
+http_response_code(200);
+echo($data[0]);
+
---
+++ updateTask.php
@@ -0,0 +1,56 @@
+<?php
+
+if(isset($_SERVER["HTTP_ORIGIN"]))
+{
+    // You can decide if the origin in $_SERVER['HTTP_ORIGIN'] is something you want to allow, or as we do here, just allow all
+    header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
+}
+else
+{
+    //No HTTP_ORIGIN set, so we allow any. You can disallow if needed here
+    header("Access-Control-Allow-Origin: *");
+}
+
+header("Access-Control-Allow-Credentials: true");
+header("Access-Control-Max-Age: 600");    // cache for 10 minutes
+
+if($_SERVER["REQUEST_METHOD"] == "OPTIONS")
+{
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_METHOD"]))
+        header("Access-Control-Allow-Methods: POST, GET, OPTIONS, DELETE, PUT"); //Make sure you remove those you do not want to support
+
+    if (isset($_SERVER["HTTP_ACCESS_CONTROL_REQUEST_HEADERS"]))
+        header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
+
+    //Just exit with 200 OK with the above headers for OPTIONS method
+    exit(0);
+}
+
+$task_name = $_POST["name"];
+$user_id = $_POST["user_id"];
+$state = $_POST["state"];
+$query = "UPDATE task SET state = {$state} WHERE name = \"{$task_name}\" AND user_id = \"{$user_id}\"";
+
+if($task_name == null && $user_id == null) {
+    http_response_code(201);
+    die();
+}
+
+$mysqlConnection = new mysqli("host","login","password","database");
+
+if($mysqlConnection -> connect_errno) {
+    http_response_code(202);
+    die();
+}
+
+$result = $mysqlConnection->query($query);
+
+if(!$result) {
+    echo($mysqlConnection->error);
+    $mysqlConnection->close();
+    http_response_code(203);
+    die();
+}
+
+$mysqlConnection->close();
+http_response_code(200);
